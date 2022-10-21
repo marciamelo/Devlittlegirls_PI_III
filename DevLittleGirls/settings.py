f78a9901a8d19ec585f6035d9204a118ba8c25ba
marciamelo <msouzademelo@yahoo.com.br>
2022-10-21 18:31:57 -0300

    Atualiza requirements.txt

--- DevLittleGirls/settings.py
+++ DevLittleGirls/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 import os.path
 from pathlib import Path
-import django_heroku
+import django_on_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -139,4 +139,4 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'DevLittleGirls_App/static') #MEDIA_ROOT = o
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
-django_heroku.settings(locals())
+django_on_heroku.settings(locals())
